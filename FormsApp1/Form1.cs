87e5c9a31be752e3e366161256b6b475d5050457
Pszlachet <10063652@mydbs.ie>
2018-04-30 20:27:54 +0100

    Added Connection String

--- FormsApp1/Form1.cs
+++ FormsApp1/Form1.cs
@@ -12,6 +12,8 @@ namespace FormsApp1
 {
     public partial class Form1 : Form
     {
+
+        static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=orderline;Integrated Security=True"; // this the only part you need from Connection String in database properties
         public Form1()
         {
             InitializeComponent();
